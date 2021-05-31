0b21930cff05ebae8ee90e67ed755ea11b4139d7
MaeveWheaton <teamheaven10@outlook.com>
2021-05-31 14:52:12 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,51 @@
+﻿
+namespace Logo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(931, 659);
+            this.ForeColor = System.Drawing.Color.LightGray;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Domino\'s Logo";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Logo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+            SolidBrush redBrush = new SolidBrush(Color.Red);
+            SolidBrush blueBrush = new SolidBrush(Color.SteelBlue);
+            SolidBrush whiteBrush = new SolidBrush(Color.White);
+            Font titleFont = new Font("Microsoft Sans Serif", 16, FontStyle.Bold);
+            
+            //left red square
+            g.TranslateTransform(100, 100);
+            g.RotateTransform(135);
+            g.FillRectangle(redBrush, 0, 0, 50, 50);
+            g.ResetTransform();
+
+            //Right red square
+            g.TranslateTransform(138, 63);
+            g.RotateTransform(135);
+            g.FillRectangle(redBrush, 0, 0, 50, 50);
+            g.ResetTransform();
+
+            //blue rectangle
+            g.TranslateTransform(176, 101);
+            g.RotateTransform(135);
+            g.FillRectangle(blueBrush, 0, 0, 103, 50);
+            g.ResetTransform();
+
+            //text
+            g.TranslateTransform(68, 140);
+            g.RotateTransform(315);
+            g.DrawString("Domino's\nPizza", titleFont, whiteBrush, 0, 0);
+            g.ResetTransform();
+
+            //left two dots
+            g.FillEllipse(whiteBrush,  42, 92, 16, 16);
+            g.FillEllipse(whiteBrush, 68, 92, 16, 16);
+
+            //right dot
+            g.FillEllipse(whiteBrush, 94, 56, 16, 16);
+        }
+    }
+}
